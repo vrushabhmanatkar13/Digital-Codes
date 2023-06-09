c897d5fb2b682f086f7bf664413d6ce64e20e87b
Vrushabh_Manatkar <vrushabhmanatkar13@gmail.com>
2023-06-09 23:30:10 +0530

    Create ASTM test cases

--- src/test/java/com/Digitalcodes/pageobject/TableOfContent_Page.java
+++ src/test/java/com/Digitalcodes/pageobject/TableOfContent_Page.java
@@ -85,6 +85,11 @@ public class TableOfContent_Page extends Baseclass{
 	@FindBy(xpath = "//div[@class='white v-card v-sheet v-sheet--outlined theme--dark']//p")
 	private WebElement notPremiumtext;
 	
+	//ASTM chapter Name
+	@FindBy(xpath = "//p[@class='mb-0 py-2 body-2 pl-2']/a")
+	private WebElement chapterName;
+	
+	
 	public void clickCodesSection() {
 		  codessection.click();
 	  }
@@ -187,5 +192,9 @@ public class TableOfContent_Page extends Baseclass{
 		String text=getText(titleName);
 		click(crossicon);
 		return text;
+	}
+	
+	public void clickChapter() {
+		click(chapterName);
 	}
 }
